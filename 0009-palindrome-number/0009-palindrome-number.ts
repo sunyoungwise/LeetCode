3724602712042113e8f539c1409fc9b36711cf72
Sunyoung <155343745+sunyoungwise@users.noreply.github.com>
2024-01-02 08:18:40 +0900

    Time: 297 ms (6.69%), Space: 51.5 MB (73.80%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,15 +1,11 @@
 function isPalindrome(x: number): boolean {
     const str = String(x);
-    let front = 0;
-    let rear = str.length - 1;
     
-    while (str.length / 2 > front) {
-        if (str[front] === str[rear]) {
-            front++;
-            rear--;
-        } else {
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
             return false;
         }
+        
     }
     
     return true;
