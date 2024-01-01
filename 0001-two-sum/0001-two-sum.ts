cd8b0d8c3f938f8e1480fc5ebbf6b752425d47c0
Sunyoung <155343745+sunyoungwise@users.noreply.github.com>
2024-01-01 15:09:49 +0900

    Time: 115 ms (21.54%), Space: 43.9 MB (90.10%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.ts
@@ -0,0 +1,13 @@
+function twoSum(nums: number[], target: number): number[] {
+    let myMap = {}; 
+    for (let i = 0; i < nums.length; i++) {
+        const x = target - nums[i];
+        if (myMap[x] !== undefined) {
+            return [i, myMap[x]];
+        } else {
+            myMap[nums[i]] = i;
+        }
+    }
+
+    return null;
+};
